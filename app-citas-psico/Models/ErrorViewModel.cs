e40720d7dcc03145586c87ad0a0918f1e0a3c4fb
Alexander Gomez <mpalexanderg@gmail.com>
2025-05-16 14:38:43 -0500

    Se agrego template Admin LTE y se implementó en layout.

---
+++ app-citas-psico/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace app_citas_psico.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
